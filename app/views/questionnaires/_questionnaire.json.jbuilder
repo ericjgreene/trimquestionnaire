0bb92e572dc65fae5dce68b51a8d18f3310daeca
Eric Greene <ericjgreene@gmail.com>
2017-01-05 14:22:23 -0500

    scaffold before migrate

---
+++ app/views/questionnaires/_questionnaire.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! questionnaire, :id, :name, :email, :projectrepo, :projecturl, :usingbestpracticesforopp, :modulardevelopment, :created_at, :updated_at
+json.url questionnaire_url(questionnaire, format: :json)
---
+++ db/migrate/20170105192125_create_questionnaires.rb
@@ -0,0 +1,14 @@
+class CreateQuestionnaires < ActiveRecord::Migration[5.0]
+  def change
+    create_table :questionnaires do |t|
+      t.string :name
+      t.string :email
+      t.string :projectrepo
+      t.string :projecturl
+      t.integer :usingbestpracticesforopp
+      t.integar :modulardevelopment
+
+      t.timestamps
+    end
+  end
+end
