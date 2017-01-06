a6279111c799d16f4c0d558e233e352b13c737f3
Eric Greene <ericjgreene@gmail.com>
2017-01-06 16:46:11 -0500

    more mailer mods

--- test/mailers/previews/t_mailer_preview.rb
+++ test/mailers/previews/t_mailer_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/t_mailer
 class TMailerPreview < ActionMailer::Preview
-
+  def welcome_email
+		TMailer.welcome_email(Questionnaire.first)
+  end
 end
