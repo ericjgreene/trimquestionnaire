db0420e82ca7b43e40fbb81d317f9f7655e1f659
Eric Greene <ericjgreene@gmail.com>
2017-01-06 16:41:34 -0500

    mailer view1 and rb1

--- app/mailers/t_mailer.rb
+++ app/mailers/t_mailer.rb
@@ -1,2 +1,9 @@
 class TMailer < ApplicationMailer
+  default from: ‘notifications@example.com’
+
+  def welcome_email(user)
+    @user = user
+    @url = ‘http://example.com/login’
+    mail(to: @user.email, subject: ‘Welcome!’)
+  end
 end
