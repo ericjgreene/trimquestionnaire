fdbbed491d253b7e771cabb7a45f20b25795191b
Eric Greene <ericjgreene@gmail.com>
2017-01-06 22:21:03 -0500

    mailers preview working

--- app/mailers/t_mailer.rb
+++ app/mailers/t_mailer.rb
@@ -1,9 +1,9 @@
 class TMailer < ApplicationMailer
-  default from: ‘machinegreene@gmail.com’
+  default :from => "machinegreene@gmail.com"
 
   def welcome_email(questionnaire)
     @questionnaire = questionnaire
-    @url = ‘http:www.google.com’
-    mail(to: @questionnaire.email, subject: ‘Welcome!’)
+    # @url = ‘http://google.com’
+    mail :to => "@questionnaire.email", :subject => "Welcome!"
   end
 end
